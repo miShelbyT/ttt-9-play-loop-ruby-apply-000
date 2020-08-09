7233ae5be3f64c40174ed994039a2356873685ca
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-09 13:46:33 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,7 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-    #until board == ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
-    #turn(board)
-  #end
+    counter = 0
+    until counter = 9
+    turn(board)
+    counter += 1
+  end
 end
